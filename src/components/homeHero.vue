aba13f963776450bdf6e7fdaab993ade9c9f5107
ykwkota <yukawa@kde.cs.tsukuba.ac.jp>
2020-09-04 18:14:02 +0900

    add homeHero components

--- src/components/homeHero.vue
+++ src/components/homeHero.vue
@@ -1,1 +1,25 @@
+<template>
+  <v-parallax v-bind:src="assetsImage">
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col class="text-center" cols="12">
+        <h1 class="display-1 font-weight-thin mb-4">VTour</h1>
+        <h4 class="subheading">コロナ渦における新たな旅行のあり方の提案</h4>
+      </v-col>
+    </v-row>
+  </v-parallax>
+</template>
 
+<script>
+export default {
+  data() {
+    return {
+      assetsImage: require('@/assets/homeHero.jpg')
+      // assetsImage_NG: "../assets/img-assets.png",
+      // staticImage: "/img-static.png"
+    };
+  }
+};
+</script>
