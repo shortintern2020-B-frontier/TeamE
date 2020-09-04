ff9e4b88265c411914246240416127f27af97501
ykwkota <yukawa@kde.cs.tsukuba.ac.jp>
2020-09-04 19:04:51 +0900

    fix title position

--- src/components/homeHero.vue
+++ src/components/homeHero.vue
@@ -1,14 +1,13 @@
 <template>
-  <v-parallax v-bind:src="assetsImage">
-    <v-row
-      align="center"
-      justify="center"
-    >
-      <v-col class="text-center" cols="12">
-        <h1 class="display-1 font-weight-thin mb-4">VTour</h1>
-        <h4 class="subheading">コロナ渦における新たな旅行のあり方の提案</h4>
-      </v-col>
-    </v-row>
+  <v-parallax dark v-bind:src="assetsImage">
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col align-self="auto">
+            <h1 class="display-1 font-weight-thin mb-4">VTour</h1>
+            <h4 class="subheading">コロナ渦における新たな旅行のあり方の提案</h4>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-parallax>
 </template>
 
@@ -17,8 +16,6 @@ export default {
   data() {
     return {
       assetsImage: require('@/assets/homeHero.jpg')
-      // assetsImage_NG: "../assets/img-assets.png",
-      // staticImage: "/img-static.png"
     };
   }
 };
