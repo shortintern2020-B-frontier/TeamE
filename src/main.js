88828ab27687297fa07c2ec500bf445667e42e01
Ryo Omae <matcikun@gmail.com>
2020-09-04 13:51:00 +0900

    routerのindex.jsからmain.jsに変更

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false
+
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'APIKEY',
+    libraries: 'places',
+    region: 'JP',
+    language: 'ja'
+  }
+})
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,9 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <head/>
+    <sample></sample>
   </div>
+  
 </template>
 
 <script>
