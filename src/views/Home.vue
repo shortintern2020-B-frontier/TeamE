9506d566fa83e29f180971ad9bdb9d004d682227
ykwkota <yukawa@kde.cs.tsukuba.ac.jp>
2020-09-04 18:12:50 +0900

    add vuetify plugin

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <head/>
+    <homeHero></homeHero>
     <sample></sample>
   </div>
   
@@ -11,12 +12,14 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 import sample from '@/components/sample.vue'
+import homeHero from '@/components/homeHero.vue'
 
 export default {
   name: 'Home',
   components: {
     // HelloWorld,
-    sample
+    sample,
+    homeHero
   }
 }
 </script>
