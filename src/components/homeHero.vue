bcc23dca35575e9af1e16c8092fd14c73eeade5d
ykwkota <yukawa@kde.cs.tsukuba.ac.jp>
2020-09-04 19:10:48 +0900

    fix font size

--- src/components/homeHero.vue
+++ src/components/homeHero.vue
@@ -1,14 +1,16 @@
 <template>
+<v-app>
   <v-parallax dark v-bind:src="assetsImage">
     <v-container fluid>
       <v-row align="center" justify="center">
         <v-col align-self="auto">
-            <h1 class="display-1 font-weight-thin mb-4">VTour</h1>
-            <h4 class="subheading">コロナ渦における新たな旅行のあり方の提案</h4>
+            <h1 class="display-4 font-weight-bold mb-4">VTour</h1>
+            <h4 class="title">コロナ渦における新たな旅行のあり方の提案</h4>
         </v-col>
       </v-row>
     </v-container>
   </v-parallax>
+</v-app>
 </template>
 
 <script>
