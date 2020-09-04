0bf2a03553dd96a4aee662ca0b77aa11e6071892
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-09-04 14:51:49 +0900

    navBar styleの変更

---
+++ src/components/navBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <div>VTour</div>
+  </header>
+</template>
+
+<style type="text/css">
+header {
+  background-color: red;
+  width: 100%;
+  height: 120px;
+}
+
+div {
+  text-align: center;
+  padding-left: 5%;
+  font-size: 24px;
+  color: black;
+}
+</style>
+
+
+
+
+
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <head/>
-    <sample></sample>
-  </div>
-  
+<div class="home">
+  <img alt="Vue logo" src="../assets/logo.png" />
+  <head />
+  <sample></sample>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-import sample from '@/components/sample.vue'
+import sample from "@/components/sample.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     // HelloWorld,
     sample
   }
-}
+};
 </script>
