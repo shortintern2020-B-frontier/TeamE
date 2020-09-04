2152f502a91ba98a54c83f5ea7a6648c5e902cf5
Ryo Omae <matcikun@gmail.com>
2020-09-04 12:44:27 +0900

    added google map

---
+++ src/components/TravelMap.vue
@@ -0,0 +1,31 @@
+<template>
+    <GoogleMapLoader
+        :mapConfig='mapConfig'
+        apiKey='AIzaSyAqchniiuahLqMZP9-WOdvo_w5pMQ6uGhQ'    
+    >
+    <template slot-scope="{google, map}">
+        {{map}}
+        {{google}}
+    </template>
+    </GoogleMapLoader>
+</template>
+
+<script>
+import GoogleMapLoader from './GoogleMapLoader'
+import { mapSettings } from '@/constants/mapSettings'
+
+export default {
+  components: {
+    GoogleMapLoader
+  },
+
+  computed: {
+    mapConfig () {
+      return {
+        ...mapSettings,
+        center: { lat: 0, lng: 0 }
+      }
+    },
+  },
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <GoogleMapLoader/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import GoogleMapLoader from '@/components/GoogleMapLoader.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld,
+    GoogleMapLoader
   }
 }
 </script>
