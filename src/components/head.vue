ee08a21771fdeff1be2a44cf719a54789b86d8ee
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-09-04 14:34:37 +0900

    navBarを追加

---
+++ src/components/head.vue
@@ -0,0 +1,15 @@
+<template>
+  <header 
+  styel="width: 100%; height=120px">
+    <div
+    style="text-align: center; padding-left: 5%">
+      VTour
+    </div>
+  </header>
+</template>
+
+
+
+
+
+
