ad25f908ebe671daa39013e166595e33b7c7a7db
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-09-04 16:01:27 +0900

    navBar home.vueに追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
 <div class="home">
+  <navBar></navBar>
   <img alt="Vue logo" src="../assets/logo.png" />
   <head />
   <sample></sample>
@@ -10,12 +11,14 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 import sample from "@/components/sample.vue";
+import navBar from "@/components/navBar.vue";
 
 export default {
   name: "Home",
   components: {
     // HelloWorld,
-    sample
+    sample,
+    navBar
   }
 };
 </script>
