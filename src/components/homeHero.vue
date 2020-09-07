febf03cb68731e27495435075a194bf1c68420be
ykwkota <yukawa@kde.cs.tsukuba.ac.jp>
2020-09-07 15:37:46 +0900

    fix search form position

--- src/components/homeHero.vue
+++ src/components/homeHero.vue
@@ -7,22 +7,16 @@
           <h4 class="title">コロナ渦における新たな旅行のあり方の提案</h4>
         </v-col>
       </v-row>
-      <v-row align="center" justify="center">
-        <v-col align-self="auto">
-            <div class="cp_iptxt">
-              <v-form ref="form">
-                <v-text-field
-                  v-model="model"
-                  :counter="max"
-                  :rules="rules"
-                  background-color="white"
-                  label="search"
-                ></v-text-field>
-              </v-form>
-            </div>
-        </v-col>
-      </v-row>
     </v-container>
+      <v-form ref="form">
+        <v-text-field
+          v-model="model"
+          :counter="max"
+          :rules="rules"
+          background-color="white"
+          label="search"
+        ></v-text-field>
+      </v-form>
   </v-parallax>
 </template>
 
