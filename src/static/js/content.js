6f1a079b7792d2947e52bde60b3b3948cdd97f6c
hiragi-kaede <zoro18782ryt131@gmail.com>
2020-09-07 15:55:29 +0900

    content.jsに分割

--- src/static/js/content.js
+++ src/static/js/content.js
@@ -4,9 +4,36 @@
 // コンテンツ取得処理
 // lng, lat, title, url
 function getContent(database){
-    
+    var contentRef = database.ref('content/');
+    return contentRef;
 }
 
 function makeContent(database, contentData) {
     var newContentRef = database.ref('')
+}
+
+//Ryota Watanabe 2020/09/07
+function SetContentMarkers(contentData){
+    //console.log(contentData);
+    Object.keys(contentData).forEach(function(key){
+      const newMarker = new google.maps.Marker({
+      position: {
+        lat: contentData[key]["lat"],
+        lng: contentData[key]["lng"]
+      },
+      map,
+      icon:
+        "../img/tokyo_2021.png",
+      title: contentData[key]["title"],
+      url:contentData[key]["url"]
+      });
+      markers.push(newMarker);
+    });
+ 
+    //Meisho Kanaomi
+    for(let i=0; i<markers.length; i++){
+        //console.log(markers[i]);
+        google.maps.event.addListener(markers[i], 'click', (function(url){
+        return window.open(markers[i].url);}))
+    }
 }
