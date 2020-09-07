634bace0b77cb1f8caeaed3d47f3ceb728a3a76e
meciwo <shanshan0474@gmail.com>
2020-09-07 13:27:12 +0900

    フィルを分割, srcの読み込むはまだ

---
+++ src/static/js/map_setting.js
@@ -0,0 +1,48 @@
+function toggleStreetView() {
+    const toggle = panorama.getVisible();
+    
+    if (toggle == false) {
+    panorama.setVisible(true);
+    showMarkers();
+    } else {
+    panorama.setVisible(false);
+    hideMarkers();
+    }
+}
+function togglePins() {
+    const toggle = panorama.getVisible();
+
+    //パノラマ状態じゃないときは変更しない
+    if(toggle == false){
+        return;
+    }
+
+    if (PinIsVisible == false) {
+      showMarkers();
+      PinIsVisible = true;
+    } else {
+      hideMarkers();
+      PinIsVisible = false;
+    }
+  }
+
+function setMapOnAll(map){
+    for(let i=0; i<markers.length; i++){
+        markers[i].setMap(map);
+    }
+}
+
+//markerを非表示にする
+function hideMarkers(){
+    setMapOnAll(null);
+}
+//markerを表示する
+function showMarkers(){
+    setMapOnAll(map);
+}
+
+//markerを消去する
+function deleteMarkers(){ 
+    hideMarkers();
+    markers=[];
+}
