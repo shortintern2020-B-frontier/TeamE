82ac36928293ad969eb22b612024a1ddf8aabf43
ykwkota <yukawa@kde.cs.tsukuba.ac.jp>
2020-09-07 11:00:30 +0900

    add dtailHero component to detail page

---
+++ src/views/Detail.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="detail">
+    <detailHero/>
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+import detailHero from "@/components/detailHero.vue";
+
+export default {
+  name: 'Detail',
+  components: {
+    detailHero
+  }
+}
+</script>
