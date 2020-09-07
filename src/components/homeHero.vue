2d3b66bbdf82675195fb44b5573b5a221951c94c
ykwkota <yukawa@kde.cs.tsukuba.ac.jp>
2020-09-07 10:48:17 +0900

    fix search form color

--- src/components/homeHero.vue
+++ src/components/homeHero.vue
@@ -1,21 +1,27 @@
 <template>
   <v-parallax dark v-bind:src="assetsImage">
     <v-container fluid>
-      <v-row align="center" justify="center">
-        <v-col align-self="auto">
+      <v-row align="center" justify="center" align-content="center">
+        <v-col class="text-center">
           <h1 class="display-4 font-weight-bold mb-4">VTour</h1>
           <h4 class="title">コロナ渦における新たな旅行のあり方の提案</h4>
         </v-col>
       </v-row>
       <v-row align="center" justify="center">
-      <v-col align-self="auto">
-          <div class="cp_iptxt">
-            <input class="ef" type="text" placeholder="">
-            <label>SEARCH</label>
-            <span class="focus_line"><i></i></span>
-          </div>
-          </v-col>
-          </v-row>
+        <v-col align-self="auto">
+            <div class="cp_iptxt">
+              <v-form ref="form">
+                <v-text-field
+                  v-model="model"
+                  :counter="max"
+                  :rules="rules"
+                  background-color="white"
+                  label="search"
+                ></v-text-field>
+              </v-form>
+            </div>
+        </v-col>
+      </v-row>
     </v-container>
   </v-parallax>
 </template>
@@ -29,94 +35,3 @@ export default {
   }
 };
 </script>
-
-<style>
-  .cp_iptxt {
-    position: relative;
-    text-align: center;
-    width: 50%;
-    margin: 40px 25%;
-  }
-  .cp_iptxt input[type='text'] {
-    font: 15px/24px sans-serif;
-    box-sizing: border-box;
-    width: 100%;
-    letter-spacing: 1px;
-    padding-left: 4em;
-  }
-  .cp_iptxt input[type='text']:focus {
-    outline: none;
-  }
-  .ef {
-    padding: 7px 14px;
-    transition: 0.4s;
-    border: 1px solid #1b2538;
-    background: white;
-  }
-  .ef ~ .focus_line:before,
-  .ef ~ .focus_line:after {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 0;
-    height: 2px;
-    content: '';
-    transition: 0.3s;
-    background-color: blue;
-  }
-  .ef ~ .focus_line:after {
-    top: auto;
-    right: 0;
-    bottom: 0;
-    left: auto;
-  }
-  .ef ~ .focus_line i:before,
-  .ef ~ .focus_line i:after {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 2px;
-    height: 0;
-    content: '';
-    transition: 0.4s;
-    background-color: blue;
-  }
-  .ef ~ .focus_line i:after {
-    top: auto;
-    right: 0;
-    bottom: 0;
-    left: auto;
-  }
-  .ef:focus ~ .focus_line:before,
-  .ef:focus ~ .focus_line:after,
-  .cp_iptxt.ef ~ .focus_line:before,
-  .cp_iptxt.ef ~ .focus_line:after {
-    width: 100%;
-    transition: 0.3s;
-  }
-  .ef:focus ~ .focus_line i:before,
-  .ef:focus ~ .focus_line i:after,
-  .cp_iptxt.ef ~ .focus_line i:before,
-  .cp_iptxt.ef ~ .focus_line i:after {
-    height: 100%;
-    transition: 0.4s;
-  }
-  .ef ~ label {
-    position: absolute;
-    z-index: -1;
-    top: 10px;
-    left: 14px;
-    width: 100%;
-    transition: 0.3s;
-    letter-spacing: 0.5px;
-    color: #aaaaaa;
-  }
-  .ef:focus ~ label,
-  .cp_iptxt.ef ~ label {
-    font-size: 12px;
-    top: -18px;
-    left: 0;
-    transition: 0.3s;
-    color: white;
-  }
-</style>
