9e05cae89e90fef374477472294b9cdc1fcd5042
ykwkota <yukawa@kde.cs.tsukuba.ac.jp>
2020-09-08 16:35:11 +0900

    change dynamic packageDetail page

--- src/components/packageDetail.vue
+++ src/components/packageDetail.vue
@@ -3,24 +3,17 @@
 <template>
   <div class="package-detail">
     <div class="title-container">
-      <h2>{{ contentTitle }}</h2>
+      <h2>{{ packagedata.package_name }}</h2>
     </div>
     <div class="detail-container">
       <h6>体験できること</h6>
-      <p>{{ contentAbout }}</p>
+      <p>{{ packagedata.package_detail }}</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    contentTitle: "京都の名所巡りの旅",
-    startPointName: "四条河原町",
-    endPointName: "金閣寺",
-    contentAbout:
-      "京都へ来たなら一度は訪れておきたいのがここ！「清水の舞台から飛び降りる」の語源にもなった人気No1の清水寺、金箔で覆われた舎利殿が見事な金閣寺(鹿苑寺)、東山文化の代表格で風情溢れる銀閣寺(慈照寺)は「京都三大名所」と呼ばれ、数ある京都の観光名所の中でも圧倒的な人気を誇っています。じっくり1ヶ所を訪れるもよし…嵐山や伏見稲荷大社などとセットで訪れるのもよし…時間がないけど全てまわりたい方には、3ヶ所全てを1日で巡る周遊ツアーがおすすめです。京都の名所を上手に押さえて、満足のいく京都旅をお楽しみください。"
-  }),
   props: ['packagedata']
 };
 </script>
