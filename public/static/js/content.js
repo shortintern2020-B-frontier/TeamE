d1a0a85a5f604a3791ff4232dfb28eaef46d5d0d
meciwo <shanshan0474@gmail.com>
2020-09-09 14:01:42 +0900

    add figs and give paths

--- public/static/js/content.js
+++ public/static/js/content.js
@@ -3,48 +3,52 @@
 
 // コンテンツ取得処理
 // lng, lat, title, url
-function getContent(database){
-    var contentRef = database.ref('content/');
-    return contentRef;
+function getContent(database) {
+  var contentRef = database.ref('content/');
+  return contentRef;
 }
 
 function addContentToDatabase(database, contentData) {
-    var newContentRef = database.ref('')
+  var newContentRef = database.ref('')
 }
 
 //Ryota Watanabe 2020/09/07
-function SetContentMarkers(contentData, markers){
-    //console.log(contentData);
-    Object.keys(contentData).forEach(function(key){
-      const newMarker = new google.maps.Marker({
+function SetContentMarkers(contentData, markers) {
+  //console.log(contentData);
+  Object.keys(contentData).forEach(function (key) {
+    const newMarker = new google.maps.Marker({
       position: {
         lat: contentData[key]["lat"],
         lng: contentData[key]["lng"]
       },
       map,
-      icon:
-        "../img/tokyo_2021.png",
+      //Meisho.Kanaomi 2020/9/9
+      icon: {
+        url: contentData[key]["img"],
+        scaledSize: new google.maps.Size(100, 100)
+      },
       title: contentData[key]["title"],
-      url:contentData[key]["url"]
-      });
-      //meisho.k 2020/9/7
-      content_markers.push(newMarker);
+      url: contentData[key]["url"]
     });
-    //urlを埋め込む
-    //Meisho Kanaomi 2020/9/4 
-    for(let i=0; i<content_markers.length; i++){
-        //console.log(markers[i]);
-        google.maps.event.addListener(content_markers[i], 'click', (function(url){
-        return window.open(content_markers[i].url);}))
-    }
+    //Meisho.kanaomi 2020/9/7
+    content_markers.push(newMarker);
+  });
+  //urlを埋め込む
+  //Meisho Kanaomi 2020/9/4 
+  for (let i = 0; i < content_markers.length; i++) {
+    //console.log(markers[i]);
+    google.maps.event.addListener(content_markers[i], 'click', (function (url) {
+      return window.open(content_markers[i].url);
+    }))
+  }
 }
 
 //Markerの初期配置・情報更新を行う
-var contentRef=getContent(DataBase);
+var contentRef = getContent(DataBase);
 var contentData;
 var contentTempData;
 //Markerの初期配置
-contentRef.once('value',function(snapshot){
+contentRef.once('value', function (snapshot) {
   console.log('contentref once')
   contentTempData = snapshot.val();
   SetContentMarkers(contentTempData, content_markers);
