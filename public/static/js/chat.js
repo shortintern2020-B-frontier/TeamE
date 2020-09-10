eded43f9fec22d087fb22df80baddbab36e8e4fd
hiiragi-kaede <zoro18782ryt131@gmail.com>
2020-09-10 10:50:02 +0900

    バグフィックス

--- public/static/js/chat.js
+++ public/static/js/chat.js
@@ -5,7 +5,6 @@ const message = document.getElementById("message");
 const msg = document.getElementById("messages");
 const chatHistory = document.getElementById("messageBody");
 
-var commentid = "-MGgFpByYA0l9TQ_MTXb";
 var str = "";
 
 const remove_comments = document.getElementById("remove_comments");
@@ -53,9 +52,6 @@ chatRef.once("value", function(snapshot) {
   if (chatData == undefined) return;
 
   Object.keys(chatData).forEach(function(key) {
-    // console.log("date:"+chatData[key]["date"]);
-    // console.log("name:"+chatData[key]["name"]);
-    // console.log("comment:"+chatData[key]["comment"]);
     let str1 =
       "<div class='nameMessa'>" +
       "<div style='font-size:7px;position:relative;top:18px;display:flex;font-weight:bold;'>" +
@@ -76,29 +72,6 @@ chatRef.once("value", function(snapshot) {
   chatHistory.scrollTop = chatHistory.scrollHeight;
 });
 
-Object.keys(chatData).forEach(function(key) {
-  // console.log("date:"+chatData[key]["date"]);
-  // console.log("name:"+chatData[key]["name"]);
-  // console.log("comment:"+chatData[key]["comment"]);
-  let str1 =
-    "<div class='nameMessa'>" +
-    "<div style='font-size:7px;position:relative;top:18px;display:flex;font-weight:bold;'>" +
-    "<div style='width:150px;word-wrap:break-word;'>" +
-    chatData[AddedKey]["name"] +
-    "</div>" +
-    "<div align='center' style='width:200px;color:#00008a;'>" +
-    chatData[AddedKey]["date"] +
-    "</div>" +
-    "</div>" +
-    "<p style='border-radius:10px;background-color:#f5f2f0;width:200px;padding:1em;display:inline-block;vertical-align:top;'>" +
-    chatData[AddedKey]["comment"] +
-    "</p>" +
-    "</div>";
-  str += str1;
-  msg.innerHTML = str;
-  //console.log("str1:"+str1)
-  chatHistory.scrollTop = chatHistory.scrollHeight;
-});
 
 //コメントの表示更新機能
 var chatRef = DataBase.ref("chat/" + roomId);
@@ -112,15 +85,19 @@ chatRef.on("value", function(snapshot) {
   var AddedKey = Object.keys(chatData).slice(-1);
   const msg = document.getElementById("messages");
   let str1 =
-    //   "<div class='message-item1'><div class='img-item'><img src=" +
-    //   v.icon +
-    "<div class='nameMessa'><div class='name-item'>" +
-    chatData[AddedKey]["name"] +
-    "</div><div class='content-item'>" +
-    chatData[AddedKey]["comment"] +
-    "</div></div><div class='time-item'>" +
-    chatData[AddedKey]["date"] +
-    "</div></div>";
+    "<div class='nameMessa'>" +
+      "<div style='font-size:7px;position:relative;top:18px;display:flex;font-weight:bold;'>" +
+        "<div style='width:150px;word-wrap:break-word;'>" +
+        chatData[AddedKey]["name"] +
+        "</div>" +
+        "<div align='center' style='width:200px;color:#00008a;'>" +
+          chatData[AddedKey]["date"] +
+        "</div>" +
+      "</div>" +
+      "<p style='border-radius:10px;background-color:#f5f2f0;width:200px;padding:1em;display:inline-block;vertical-align:top;'>" +
+        chatData[AddedKey]["comment"] +
+      "</p>" +
+    "</div>";
   str += str1;
   msg.innerHTML = str;
   //console.log("str1:"+str1)
